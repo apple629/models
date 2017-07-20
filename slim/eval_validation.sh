c6330ca4fc064787a69f35e6da310e200d9368aa
isiosia <isiosia@gmail.com>
2017-07-20 14:51:12 +0800

    allow loop eval

--- slim/eval_validation.sh
+++ slim/eval_validation.sh
@@ -5,6 +5,7 @@ python -u eval_image_classifier.py \
   --dataset_split_name=validation \
   --model_name=inception_v4 \
   --checkpoint_path=$TRAIN_DIR \
-   --eval_dir=/tmp/eval/validation \
+  --eval_dir=/tmp/eval/validation \
+  --eval_interval_secs=60 \
   --batch_size=32 
 
