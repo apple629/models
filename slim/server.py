0d600f08be30181cfe396d4ae478b85ff7cb9559
isiosia <isiosia@gmail.com>
2017-07-25 11:28:53 +0800

    允许sess重用

--- slim/server.py
+++ slim/server.py
@@ -8,7 +8,9 @@ from flask import request, send_from_directory
 from flask import Flask, request, redirect, url_for
 import uuid
 import tensorflow as tf
-from classify_image import run_inference_on_image
+import numpy as np
+#from classify_image import run_inference_on_image
+from classify_image import NodeLookup
 
 ALLOWED_EXTENSIONS = set(['jpg','JPG', 'jpeg', 'JPEG', 'png'])
 
@@ -41,9 +43,35 @@ def rename_filename(old_file_name):
   new_name = str(uuid.uuid1()) + ext
   return new_name
 
+
+def init_graph(model_name=FLAGS.model_name):
+  with open(model_name, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+def run_inference_on_image(file_name):
+  image_data = open(file_name, 'rb').read()
+  sess = app.sess
+  softmax_tensor = sess.graph.get_tensor_by_name('InceptionV4/Logits/Predictions:0')
+  predictions = sess.run(softmax_tensor,
+                           {'input:0': image_data})
+  predictions = np.squeeze(predictions)
+
+  # Creates node ID --> English string lookup.
+  node_lookup = app.node_lookup
+  top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
+  top_names = []
+  for node_id in top_k:
+    human_string = node_lookup.id_to_string(node_id)
+    top_names.append(human_string)
+    score = predictions[node_id]
+    print('id:[%d] name:[%s] (score = %.5f)' % (node_id, human_string, score))
+  return predictions, top_k, top_names
+
 def inference(file_name):
   try:
-    predictions, top_k, top_names = run_inference_on_image(file_name, model_file=FLAGS.model_name)
+    predictions, top_k, top_names = run_inference_on_image(file_name)
     print(predictions)
   except Exception as ex: 
     print(ex)
@@ -80,11 +108,21 @@ def root():
       file.save(file_path)
       type_name = 'N/A'
       print('file saved to %s' % file_path)
+      start_time = time.time()
       out_html = inference(file_path)
+      duration = time.time() - start_time
+      print('duration:[%.0fms]' % (duration*1000))
       return result + out_html 
   return result
 
 if __name__ == "__main__":
   print('listening on port %d' % FLAGS.port)
+  init_graph(model_name=FLAGS.model_name)
+  label_file, _ = os.path.splitext(FLAGS.model_name)
+  label_file = label_file + '.label'
+  node_lookup = NodeLookup(label_file)
+  app.node_lookup = node_lookup
+  sess = tf.Session()
+  app.sess = sess
   app.run(host='0.0.0.0', port=FLAGS.port, debug=FLAGS.debug, threaded=True)
 
